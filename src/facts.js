639a7700f3ca5ab955618db2b951fa70b7b52db1
elainabaobao <lingchen@bu.edu>
2017-06-15 19:00:08 -0400

    Add files via upload
    updated facts with new themes, added new intents and functions

--- src/facts.js
+++ src/facts.js
@@ -1,13 +1,60 @@
 const facts = {
 	"themes": {
-		"space": [
-
-		],
-		"physics": [
+	
+		"achievement": [
 			{
-				factName: "if you are sitting on opposite ends of a simple canoe with a partner, and you know the weight of the canoe and your own weight, then you can calculate how much your partner weights if you measure the displacement of the canoe as you and your partner get up and switch places."
+				factName: "You have to learn the rules of the game. And then you have to play better than anyone else.",
+				factName: "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.",
+				factName: "Happiness is that state of consciousness which proceeds from the achievement of one's values.",
+				factName: "Keep away from those who try to belittle your ambitions. Small people always do that, but the really great make you believe that you too can become great.",
+				factName: "Empty pockets never held anyone back. Only empty heads and empty hearts can do that.",
+				factName: "Desire is the starting point of all achievement, not a hope, not a wish, but a keen pulsating desire which transcends everything.",
+				factName: "Things may come to those who wait...but only the things left by those who hustle.",
+				factName: "If you put off everything till you're sure of it, you'll never get anything done.",
+				factName: "What we have once enjoyed we can never lose. All that we love deeply becomes a part of us.",
+				factName: "If you don't have time to do it right, when will you have time to do it over?",
 			}
-		]
+		],
+		"brave": [
+			{
+				factName: "a brave man, whose only fault was being a woman.",
+				factName: "Having thus chosen our course, without guile and with pure purpose, let us renew our trust in God, and go forward without fear and with manly hearts.",
+				factName: "The miracle, or the power, that elevates the few is to be found in their industry, application, and perseverance under the prompting of a brave, determined spirit.",
+				factName: "Except a creature be part coward, it is not a compliment to say he is brave; it is merely a loose misapplication of the word",
+				factName: "Except a creature be part coward it is not a compliment to say it is brave; it is merely a loose application of the word. Consider the flea! -- incomparably the bravest of all the creatures of God, if ignorance of fear were courage.",
+				factName: "Not thoughtlessly submit to hereditary prejudices but honestly and courageously uses his intelligence.",
+				factName: "I love those who can smile in trouble, who can gather strength from distress, and grow brave by reflection.",
+				factName: "Manifestly, dying is nothing to a really great and brave man",
+				factName: "Without courage all virtues lose their meaning.",
+			}
+		],
+		"determination": [
+			{
+				factName: "Never, never, never, never give up.",
+				factName: "There is only one way to succeed in anything and that is to give it everything.",
+				factName: "They cannot take away our self-respect if we do not give it to them.",
+				factName: "I never could have done what I have done without the habits of punctuality, order, and diligence, without the determination to concentrate myself on one subject at a time.",
+				factName: "I have met a lot of hardboiled eggs in my time, but you're twenty minutes.",
+				factName: "The measure of who we are is what we do with what we have.",
+				factName: "Exercise of faith will be the safest where there is a clear determination summarily to reject all that is contrary to truth and love",
+				factName: "The question isn't who is going to let me; it's who is going to stop me.",
+				factName: "Determine that the thing can and shall be done, and then we shall find the way.",
+			}
+		],
+		"motivation": [
+			{
+				factName: "Either you run the day or the day runs you.",
+				factName: "I was motivated to be different in part because I was different.",
+				factName: "Set your sights high, the higher the better. Expect the most wonderful things to happen, not in the future but right now. Realize that nothing is too good. Allow absolutely nothing to hamper you or hold you up in any way.",
+				factName: "We make the world we live in and shape our own environment.",
+				factName: "What you do today can improve all your tomorrows.",
+				factName: "Wherever you are - be all there.",
+				factName: "Edison failed 10, 000 times before he made the electric light. Do not be discouraged if you fail a few times.",
+				factName: "Problems are not stop signs, they are guidelines.",
+				factName: "Out of clutter, find Simplicity. From discord, find Harmony. In the middle of difficulty lies Opportunity."
+,
+			}
+		],
 	}
 };
 
--- src/responses.js
+++ src/responses.js
@@ -1,5 +1,5 @@
 const {themes} = require('./facts');
-const {space, physics} = themes;
+const {achievement, brave, determination, motivation} = themes;
 const initSessionAttributes = () => {
     // If we wanted to initialize the session to have some attributes we could add those here.
     const sessionAttributes = {};
@@ -7,10 +7,28 @@ const initSessionAttributes = () => {
     return sessionAttributes;
 }
 
-const getFactResponse = () => {
+// const getFactResponse = () => {
+//     console.log(themes)
+//     const cardTitle = 'Fact';
+//     const speechOutput = 'Here is a fact: ' + physics[0].factName;
+//     // If the user either does not reply to the welcome message or says something that is not
+//     // understood, they will be prompted again with this text.
+//     const repromptText = 'Ask me about x y and z ';
+//     const shouldEndSession = false;
+
+//     return Promise.resolve()
+//         .then(_ => [
+//             cardTitle,
+//             speechOutput,
+//             repromptText,
+//             shouldEndSession
+//         ]);   
+// }
+//
+const getNewAchievementIntent = () => {
     console.log(themes)
     const cardTitle = 'Fact';
-    const speechOutput = 'Here is a fact: ' + physics[0].factName;
+    const speechOutput = 'Here is a fact: ' + achievement[0].factName;
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
     const repromptText = 'Ask me about x y and z ';
@@ -24,6 +42,66 @@ const getFactResponse = () => {
             shouldEndSession
         ]);   
 }
+const getNewBraveIntent = () => {
+    console.log(themes)
+    const cardTitle = 'Fact';
+    const speechOutput = 'Here is a fact: ' + brave[0].factName;
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Ask me about x y and z ';
+    const shouldEndSession = false;
+
+    return Promise.resolve()
+        .then(_ => [
+            cardTitle,
+            speechOutput,
+            repromptText,
+            shouldEndSession
+        ]);   
+}
+const getNewDeterminationIntent = () => {
+    console.log(themes)
+    const cardTitle = 'Fact';
+    const speechOutput = 'Here is a fact: ' + determination[0].factName;
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Ask me about x y and z ';
+    const shouldEndSession = false;
+
+    return Promise.resolve()
+        .then(_ => [
+            cardTitle,
+            speechOutput,
+            repromptText,
+            shouldEndSession
+        ]);   
+}
+const getNewMotivationIntent = () => {
+    console.log(themes)
+    const cardTitle = 'Fact';
+    const speechOutput = 'Here is a fact: ' + motivation[0].factName;
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Ask me about x y and z ';
+    const shouldEndSession = false;
+
+    return Promise.resolve()
+        .then(_ => [
+            cardTitle,
+            speechOutput,
+            repromptText,
+            shouldEndSession
+        ]);   
+}
+// GetNewFactIntent': function () {
+//         var factArr = data;
+//         var factIndex = Math.floor(Math.random() * factArr.length);
+//         var randomFact = factArr[factIndex];
+//         var speechOutput = GET_FACT_MESSAGE + randomFact;
+//         this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+//     },
+
+
 
 const getWelcomeResponse = () => {
     const cardTitle = 'Welcome';
@@ -64,6 +142,14 @@ const intentRequest = (intentRequest, session) => {
     switch (intentName) {
         case 'GetNewFactIntent':
             return getFactResponse();
+        case 'GetNewAchievementIntent':
+            return getNewAchievementIntent();
+        case 'GetNewBraveIntent':
+            return getNewBraveIntent();
+        case 'GetNewDeterminationIntent':
+            return getNewDeterminationIntent();
+        case 'GetNewMotivationIntent':
+            return getNewMotivationIntent();
         case 'AMAZON.StopIntent':
         case 'AMAZON.CancelIntent':
             return getEndResponse();
